6c9b4b4d6040f3063f3d405fa4e1f90684e0bd2b
Brittany Martin <brittany.jill.martin@gmail.com>
2017-09-22 14:07:59 -0400

    Register TrustyCMS 3.0.11 and fix page part creation

--- lib/trusty_cms.rb
+++ lib/trusty_cms.rb
@@ -2,6 +2,6 @@ TRUSTY_CMS_ROOT = File.expand_path(File.join(File.dirname(__FILE__), "..")) unle
 
 unless defined? TrustyCms::VERSION
   module TrustyCms
-    VERSION = '3.0.10'
+    VERSION = '3.0.11'
   end
 end
